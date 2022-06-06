291b60939fbcd66ac1f80e5e0b59f449daa93cbb
vladuhalys <vlad.lysenko1999@gmail.com>
2022-06-07 00:32:05 +0300

    Добавьте файлы проекта.

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoApp.ViewModel
+{
+    internal class MainViewModel
+    {
+    }
+}
